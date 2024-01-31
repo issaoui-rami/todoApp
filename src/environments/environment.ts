30325719e278aa6daaaa30647d816b9edda2350d
Rami Issaoui <rami.issaoui@medianet.com.tn>
2024-01-31 14:32:18 +0100

    create todoApp

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: 'AIzaSyBs8UkWafVoYGVwwFJFSt8oSt0Nw6P09Xg',
+      authDomain: 'todo-app-a3430.firebaseapp.com',
+      projectId: 'todo-app-a3430',
+      storageBucket: 'todo-app-a3430.appspot.com',
+      messagingSenderId: '234950133944',
+      appId: '1:234950133944:web:4d9610061e61726d2bcc4d',
+    },
+  };
+  
